0700deadc2a660adef15eec0ea64d05973eb7562
shivani waskar <120095784+ShivaniWaskar@users.noreply.github.com>
2022-12-27 13:14:24 +0530

    Update steamlit_App.py
    added 3 text lines

--- steamlit_App.py
+++ steamlit_App.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healty Dinner')
+streamlit.Header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry')
+streamlit.text('Kale, Spinach')
+streamlit.text('Hard Boiled')
