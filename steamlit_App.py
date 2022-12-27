77ca5959f654554847ab8b6d7cead7c93391a8ca
shivani waskar <120095784+ShivaniWaskar@users.noreply.github.com>
2022-12-27 13:15:25 +0530

    Update steamlit_App.py
    added header

--- steamlit_App.py
+++ steamlit_App.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healty Dinner')
-streamlit.Header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and blueberry')
 streamlit.text('Kale, Spinach')
 streamlit.text('Hard Boiled')
